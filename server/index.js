280d2e444e3de36fb695ef20cac8134d9014628e
Erik Golden <erikgolden7@gmail.com>
2018-08-30 16:15:48 -0500

    added nodemailer

--- server/index.js
+++ server/index.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const cors = require('cors');
-const port = 3001;
+const nodeMailer = require('nodemailer');
 const path = require('path');
+const { urlencoded, json } = require('body-parser');
+require('dotenv').config();
+
+const port = 3001;
 
 const app = express();
+app.use(urlencoded({ extended: true }));
+app.use(json());
 app.use(cors());
-
-// app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.post('/send-email', function(req, res) {
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+
+  let mailOptions = {
+    from: `${req.body.name} ${req.body.surname} <${req.body.email}>`,
+    to: `Erik Golden <${process.env.GMAIL_USER}>`,
+    subject: req.body.subject,
+    text: req.body.message,
+    html: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.render('contact-failure');
+    } else {
+      res.redirect(process.env.HOME_REDIRECT || 'http://localhost:3001/');
+    }
+  });
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
